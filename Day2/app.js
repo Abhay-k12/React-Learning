0e2df1fc99d67454959b67b8acd38e4857913a77
Abhay-k12 <abhaykanojia51@gmail.com>
2026-03-27 17:28:35 +0530

    Addition of JavaScript Variables into JSX

--- Day2/app.js
+++ Day2/app.js
@@ -1,11 +1,42 @@
 function Heading() {
-    return (<div id="Heading"> 
-                    <center>
-                        <h1> Hello World  </h1>
-                        <h2> Welcome Here!</h2>
-                    </center> 
-                </div>);
+    return (
+        <div id="Heading"> 
+            <center>
+                <h1> Hello World  </h1>
+                <h2> Welcome Here!</h2>
+            </center> 
+        </div>
+    );
+}
+
+function ShowName(name, age) {
+    return (
+        <div>
+            <center>
+                <h1> Welcome User {name}</h1>
+                <h2>Your age accoding to us is {age}</h2>
+            </center>
+        </div>
+    );
+}
+
+function showListArray(array) {
+    return (
+        <ol>
+            {array.map((arrItem)=> { return <li>{arrItem}</li>})}
+        </ol>
+    );
 }
 
 const root = ReactDOM.createRoot( document.getElementById("root"));
-root.render(<Heading/>);
+
+const element1 = Heading();
+const element2 = ShowName('Abhay Kanojia',20);
+const element3 = showListArray(["Abhay", "Ankush", "Ankush", "Priyanshu"]);
+root.render(
+        <>
+            {element1}
+            {element2}
+            {element3}
+        </>
+    );
