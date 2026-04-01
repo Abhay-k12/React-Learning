f8303c25e29cd7f3f5635c4c2a482737c24c1396
Abhay-k12 <abhaykanojia51@gmail.com>
2026-04-01 09:01:41 +0530

    Build first react application with react & vite

---
+++ Day3/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+    return (
+        <h1>Hello World This is Main Application</h1>
+    )
+}
+
+export function Abhay() {
+    return (
+        <h1>Hello From Abhay</h1>
+    )
+}
+
+export function GraphicEra() {
+    return (
+        <h2>We are Graphic Era, Transforming Dreams Into Reality</h2>
+    )
+}
