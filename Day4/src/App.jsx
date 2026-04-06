cb8306bc66c7b58915db693523a18b01c3bb9b8b
Abhay-k12 <abhaykanojia51@gmail.com>
2026-04-06 20:42:01 +0530

    Implemented React useState hooks

--- Day4/src/App.jsx
+++ Day4/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 
 export default function App() {
-  let count = 0;
+  let [count, setCount] = useState(0);
+
+  function incrementNumber() {
+    count++;
+    setCount(count);
+  }
+
   return (
     <>
       <p>Counter : {count}</p>
-      <button id="increment">Increment</button>
+      <button onClick={incrementNumber}>Increment</button>
     </>
   )
 }
